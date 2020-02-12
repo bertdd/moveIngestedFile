26a7a6f7576ac18cf667e81b3afb72b43e5f92e9
Bert Degenhart Drenth <bertdd@me.com>
2020-02-12 13:27:17 +0100

    Add project files.

---
+++ PDFMover/FileMover.cs
@@ -0,0 +1,71 @@
+﻿using System.IO;
+
+namespace DDigitIngest
+{
+  class FileMover
+  {
+    public FileMover(string path, int filesInFolder, int folderOffset)
+    {
+      this.path = path;
+      this.filesInFolder = filesInFolder;
+      this.folderOffset = folderOffset;
+
+      if (!Directory.Exists(path))
+      {
+        throw new DirectoryNotFoundException(path);
+      }
+    }
+
+    internal int MoveFiles()
+    {
+      int count = 0;
+      var directoryInfo = new DirectoryInfo(path);
+      foreach (var file in directoryInfo.GetFiles())
+      {
+        if (MoveFile(file))
+        {
+          count++;
+        }
+      }
+      return count;
+    }
+
+    bool MoveFile(FileInfo file)
+    {
+      bool moved = false;
+      var folderNumber = ComputeFolder(file.Name);
+      if (folderNumber > 0)
+      {
+        var destination = $"{GetSubFolder(folderNumber)}{Path.DirectorySeparatorChar}{file.Name}";
+        try
+        {
+          File.Move(file.FullName, destination);
+          moved = true;
+        }
+        catch (IOException)  // ignore errors, they would be mostly file in use, ust retry next round.
+        {
+         
+        }
+      }
+      return moved;
+    }
+
+    string GetSubFolder(int folderNumber)
+    {
+      var subFolder = $"{path}{Path.DirectorySeparatorChar}{folderNumber}";
+      if (!Directory.Exists(subFolder))
+      {
+        Directory.CreateDirectory(subFolder);
+      }
+      return subFolder;
+    }
+
+    int ComputeFolder(string fileName)
+      => int.TryParse(Path.GetFileNameWithoutExtension(fileName), out int fileNumber) ?
+         (fileNumber / filesInFolder) - folderOffset : 0;
+
+    readonly string path;
+    readonly int filesInFolder;
+    readonly int folderOffset;
+  }
+}
---
+++ PDFMover/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace DDigitIngest
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      if (args.Length < 1)
+      {
+        Console.WriteLine("Usage: PDFMover parentFolder [filesPerFolder] [fileOffset]");
+        return;
+      }
+
+      if (!(args.Length > 2 && int.TryParse(args[1], out int filesInFolder) && filesInFolder > 100))
+      {
+        filesInFolder = 500;
+      }
+
+      if (!(args.Length > 3 && int.TryParse(args[2], out int fileOffset) && fileOffset > 0))
+      {
+        fileOffset = 399999;
+      }
+
+
+      try
+      {
+        mover = new FileMover(args[0], filesInFolder, fileOffset);
+
+        while (true)
+        {
+          Console.WriteLine($"{DateTime.Now} check");
+          var count = mover.MoveFiles();
+          if (count > 0)
+          {
+            Console.WriteLine($"{count} files moved");
+          }
+          Thread.Sleep(10000);
+        }
+      }
+      catch (DirectoryNotFoundException ex)
+      {
+        Console.WriteLine($"Ingest directory '{ex.Message}' not found!");
+      }
+    }
+
+    static FileMover mover;
+  }
+}
